0cd75ea83bc6549db6110ea0fa4a4e9e61ecae70
Caleb Makela <makelacaleb@gmail.com>
2016-11-30 14:54:04 -0800

    Created utility to get UI siblings, ham menu work

--- Assets/Scripts/InGameMenuManager.cs
+++ Assets/Scripts/InGameMenuManager.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.Assertions;
 
 public class InGameMenuManager : MonoBehaviour
 {
@@ -30,6 +32,16 @@ public class InGameMenuManager : MonoBehaviour
             UIUtilities.ActivateCanvasGroup(MenuOptionsPanel);
         else
             UIUtilities.DeactivateCanvasGroup(MenuOptionsPanel);
+    }
+
+    public void ToggleButtons(Button toggleButton)
+    {
+        Button[] sibling_buttons = UIUtilities.GetSiblingsOfType<Button>(toggleButton.gameObject);
+        if (sibling_buttons == null || sibling_buttons.Length == 0)
+            return;
+        bool is_active = sibling_buttons[0].gameObject.activeSelf;
+        foreach(Button sibling in sibling_buttons)
+            sibling.gameObject.SetActive(!is_active);
     }
 
     public void OpenContractManager()
--- Assets/Scripts/UIUtilities.cs
+++ Assets/Scripts/UIUtilities.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
 
 public static class UIUtilities
 {
@@ -19,5 +22,24 @@ public static class UIUtilities
         group.alpha = 0;
         group.blocksRaycasts = false;
         group.interactable = false;
+    }
+
+    public static T[] GetSiblingsOfType<T>(GameObject obj) where T : UIBehaviour
+    {
+        Transform parent = obj.transform.parent;
+        Assert.IsNotNull(parent);
+
+        GameObject[] siblings = new GameObject[parent.childCount - 1];
+
+        int i1 = 0;
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            if (parent.GetChild(i).gameObject == obj)
+                continue;
+            siblings[i1] = parent.GetChild(i).gameObject;
+            i1++;
+        }
+        
+        return siblings.Where(x => x.GetComponent<T>() != null).Select(x => x.GetComponent<T>()).ToArray();
     }
 }
